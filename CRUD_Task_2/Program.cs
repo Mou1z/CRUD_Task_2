1eb5370b4ef3ebd564825a121d9641ebeaf495d1
Aristocrat <73653063+Mou1z@users.noreply.github.com>
2024-04-04 02:51:11 +0200

    Update Program.cs

--- CRUD_Task_2/Program.cs
+++ CRUD_Task_2/Program.cs
@@ -4,7 +4,7 @@ using MySql.Data.MySqlClient;
 public class CRUD_Task_2
 {
     // Modify the connection string here
-    public static string connectionString = "server=localhost;port=7777;user=root;password=Th!sIsMyPassword;database=school;";
+    public static string connectionString = "server=___;port=___;user=___;password=___;database=___;";
 
     public static object[,] groceryItemsData = new object[,]
     {
